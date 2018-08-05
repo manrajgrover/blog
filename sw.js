2441a56b81ef95715fcecd9408b15389ac7be448
Manraj Singh Grover <manrajsinghgrover@gmail.com>
2018-08-05 13:56:03 +0530

    Updates

--- sw.js
+++ sw.js
@@ -37,7 +37,7 @@
 /* eslint-disable indent, no-unused-vars, no-multiple-empty-lines, max-nested-callbacks, space-before-function-paren, quotes, comma-spacing */
 'use strict';
 
-var precacheConfig = [["/blog/app-2f1fa748fad68b422df1.js","bb57c65842da5e610a9f76cad66579c7"],["/blog/app-4abc1410dfccf9056d48.js","b0cb9e2e2c247666bb2cdb128c0298f3"],["/blog/commons-67fdb66cd90db0868385.js","36126f2a6562d5379498aed3e2847634"],["/blog/commons-efa9510b7c8a15486d23.js","7ea65d166b123556a337965c2ed02554"],["/blog/index.html","06093706034d3662c729f328ba90376b"],["/blog/manifest.webmanifest","49f18ed442d6606913c5cb64c9f05a50"],["/blog/offline-plugin-app-shell-fallback/index.html","600371ddd80bfb866caf0bd8a291c17b"]];
+var precacheConfig = [["/blog/app-2f1fa748fad68b422df1.js","bb57c65842da5e610a9f76cad66579c7"],["/blog/app-4abc1410dfccf9056d48.js","b0cb9e2e2c247666bb2cdb128c0298f3"],["/blog/commons-67fdb66cd90db0868385.js","36126f2a6562d5379498aed3e2847634"],["/blog/commons-b0b502d4bd393a0feeef.js","d69ebf932f6a652b34ead5634fcd507e"],["/blog/commons-efa9510b7c8a15486d23.js","7ea65d166b123556a337965c2ed02554"],["/blog/index.html","4d1efc033fc54e49000fc9e6aaec62de"],["/blog/manifest.webmanifest","49f18ed442d6606913c5cb64c9f05a50"],["/blog/offline-plugin-app-shell-fallback/index.html","4a2bf455155b998b5eb3805d85d95813"]];
 var cacheName = 'sw-precache-v3-gatsby-plugin-offline-' + (self.registration ? self.registration.scope : '');
 
 

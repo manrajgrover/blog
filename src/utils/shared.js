fe6cc12a1391b23513371040f30a53ae3b9cfbdb
Manraj Singh Grover <manrajsinghgrover@gmail.com>
2018-08-07 00:37:45 +0530

    Routing: Home should route to /blog and not website main page

--- src/utils/shared.js
+++ src/utils/shared.js
@@ -1,5 +1,5 @@
 import { forceCheck } from "react-lazyload";
-import { navigateTo } from "gatsby-link";
+import { withPrefix, navigateTo } from "gatsby-link";
 
 export function featureNavigator(e) {
   e && e.preventDefault();
@@ -17,13 +17,13 @@ export function featureNavigator(e) {
           // uncomment following lines if you want to count featuring Navigator as a visit
           // to index page ('/'), you have to also uncomment import { navigateTo }...
 
-          navigateTo("/");
+          navigateTo(withPrefix("/"));
         });
       }, 300);
     } else {
       setTimeout(() => {
         this.props.setNavigatorPosition("is-featured");
-        navigateTo("/");
+        navigateTo(withPrefix("/"));
       }, 0);
     }
   }
